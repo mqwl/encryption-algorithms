e5482d6cc9b52ead90f6f0d22a7e7080919d095b
mqwl <none>
2025-11-14 23:48:34 +1000

    rsa implemenation using sympy

--- rsa.py
+++ rsa.py
@@ -1,71 +1,53 @@
-import random
 import sympy
 
 
 class RSACipher:
-    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
     @staticmethod
-    def eea(a, b):
-        old_c, c = 1, 0
-        old_d, d = 0, 1
-        while b != 0:
-            res = a // b
-            a, b = b, a - res * b
-            old_c, c = c, old_c - res * c
-            old_d, d = d, old_d - res * d
-        return a, old_c, old_d
-    @staticmethod
-    def open_exp(e):
-        while True:
-            num = random.randint(1, e)
-            if sympy.gcd(e, num) == 1:
-                return num
+    def open_key(euler):
+        for i in range(3, euler, 2):
+            candidate = sympy.gcd(i, euler)
+            if candidate == 1:
+                return i
 
     def generate_keys(self):
-        randprime_1 = sympy.ntheory.generate.randprime(2 ** (1024 - 1), 2 ** 1024)
-        randprime_2 = sympy.ntheory.generate.randprime(2 ** (1024 - 1), 2 ** 1024)
-        rand_module = randprime_1 * randprime_2
+        randprime_1 = sympy.randprime(2 ** (20 - 1), 2 ** 20)
+        randprime_2 = sympy.randprime(2 ** (20 - 1), 2 ** 20)
+        n = randprime_1 * randprime_2
         euler = (randprime_1 - 1) * (randprime_2 - 1)
-        # open_exp = RSACipher.open_exp(euler)
-        open_exp = 17
-        gcd, x, y = RSACipher.eea(open_exp, euler)
-        d = x + euler if x < 0 else x
+        e = RSACipher.open_key(euler)
+        d = sympy.mod_inverse(e, euler)
+        
         f = open('public.txt', 'w')
-        f.write(str(rand_module) + '\n')
-        f.write(str(open_exp) + '\n')
+        f.write(str(n) + '\n')
+        f.write(str(e) + '\n')
         f.close()
-        f_priv = open('private.txt', 'w')
-        f_priv.write(str(rand_module) + '\n')
-        f_priv.write(str(d) + '\n')
-        f_priv.close()
-    
+
+        f = open('private.txt', 'w')
+        f.write(str(n) + '\n')
+        f.write(str(d) + '\n')
+        f.close()
+
     def encrypt(self, text):
         f = open('public.txt', 'r')
         n = int(f.readline())
         e = int(f.readline())
         f.close()
-        res = []
-        for i in range(len(text)):
-            res.append(str(ord(text[i])))
-        res = ''.join(res)
-        print(res)
-        cipher = int(res) ** e % n
-        return cipher
-    
+        return pow(text, e, n)
+
     def decrypt(self, text):
         f = open('private.txt', 'r')
         n = int(f.readline())
         d = int(f.readline())
         f.close()
-        res = []
-        for i in range(len(text)):
-            res.append(str(ord(text[i])))
-        res = ''.join(res)
-        cipher = int(res) ** d % n
-        return cipher
+        return pow(text, d, n)
 
 
 cipher_master = RSACipher()
-#print(cipher_master.encrypt('welcome'))
 cipher_master.generate_keys()
+message = 1000000
+print(message)
+encrypt = cipher_master.encrypt(message)
+print(encrypt)
+decrypt = cipher_master.decrypt(encrypt)
+print(decrypt)
